4888c8af54894a804d21c72d34615250e7017834
GacusPL <kapiszponar123@gmail.com>
2025-12-15 02:35:10 +0100

    Lab8

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,11 +1,28 @@
 import Link from 'next/link';
+import classes from './page.module.css';
+import MealsGrid from '@/components/meals/meal-grid';
 
 export default function MealsPage() {
   return (
-    <main>
-      <h1>Meals Page</h1>
-      <p><Link href="/meals/share">Share a meal</Link></p>
-      <p><Link href="/meals/spaghetti">Spaghetti (Dynamic Test)</Link></p>
-    </main>
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created{' '}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose your favorite recipe and cook it yourself. It is easy and fun!
+        </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">
+            Share Your Favorite Recipe
+          </Link>
+        </p>
+      </header>
+
+      <main className={classes.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
   );
 }
--- components/main-header.js
+++ components/main-header.js
@@ -3,8 +3,12 @@ import Image from 'next/image';
 import logoImg from '@/assets/logo.png';
 import classes from './main-header.module.css';
 import MainHeaderBackground from './main-header-background';
+//import { usePathname } from 'next/navigation';
+import NavLink from './nav-link';
 
 export default function MainHeader() {
+  /*const path = usePathname();*/
+
   return (
     <>
       <MainHeaderBackground /> 
@@ -22,10 +26,10 @@ export default function MainHeader() {
         <nav className={classes.nav}>
           <ul>
             <li>
-              <Link href="/meals">Browse Meals</Link>
+              <NavLink href="/meals">Browse Meals</NavLink>
             </li>
             <li>
-              <Link href="/community">Foodies Community</Link>
+              <NavLink href="/community">Foodies Community</NavLink>
             </li>
           </ul>
         </nav>
