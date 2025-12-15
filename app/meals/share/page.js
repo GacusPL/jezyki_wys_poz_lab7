568fe4691e2ace472884c4e544f067c7e96ba03a
GacusPL <41446162+GacusPL@users.noreply.github.com>
2025-12-15 11:39:51 +0100

    lab 10 image picker

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,4 +1,6 @@
 import classes from './page.module.css';
+import ImagePicker from '@/components/meals/image-picker';
+import { shareMeal } from '@/lib/actions';
 
 export default function ShareMealPage() {
   return (
@@ -38,7 +40,7 @@ export default function ShareMealPage() {
               required
             ></textarea>
           </p>
-          IMAGE PICKER
+          <ImagePicker label="Your Image" name="image" />
           <p className={classes.actions}>
             <button type="submit">Share Meal</button>
           </p>
